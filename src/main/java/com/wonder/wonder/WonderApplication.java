fa309af1c153994e1d88cebb53ab460b5f7d2924
bm <b.missurenko@gmail.com>
2017-06-03 16:44:49 +0300

    origin setup

---
+++ src/main/java/com/wonder/wonder/WonderApplication.java
@@ -0,0 +1,12 @@
+package com.wonder.wonder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderApplication.class, args);
+	}
+}
