8c7ccdbbbf1c579f597ec0269d9a3fc716ec44ad
bm <b.missurenko@gmail.com>
2017-06-14 19:07:57 +0300

    Add cards enum do test maybe and logic for gold and purple and logic transfer information

--- src/main/java/com/wonder/wonder/service/cards/AllCard.java
+++ src/main/java/com/wonder/wonder/service/cards/AllCard.java
@@ -4,6 +4,7 @@ package com.wonder.wonder.service.cards;
  * Creator: bm
  * Date: 14.06.17.
  */
+// and for bd down
 public enum  AllCard {
     //Vonder
     THE_COLOSSUS_OF_RHODES,
@@ -95,4 +96,181 @@ LUMBER_YARD,
     SCIENTISTS_GUILD,
     MAGISTRATES_GUILD,
     BUILDERS_GUILD;
+
+
+    //Vonder
+//     ('THE_COLOSSUS_OF_RHODES',
+//             'THE_LIGHTHOUSE_OF_ALEXANDRIA',
+//             'THE_TEMPLE_OF_ARTEMIS_IN_EPHESUS',
+//             'THE_HANGING_GARDENS_OF_BABYLON',
+//             'THE_STATUE_OF_ZEUS_IN_OLYMPIA',
+//             'THE_MAUSOLEUM_OF_HALICARNASSUS',
+//             'THE_PYRAMIDS_OF_GIZA',
+//             // brown first
+//             'LUMBER_YARD',
+//             'STONE_PIT',
+//             'CLAY_POOL',
+//             'ORE_VEIN',
+//             // brown second age
+//             'SAWMILL',
+//             'QUARRY',
+//             'BRICKYARD',
+//             'FOUNDRY',
+//             //brown half
+//
+//             'TREE_FARM',
+//             'EXCAVATION',
+//             'CLAY_PIT',
+//             'TIMBER_YARD',
+//             'FOREST_CAVE',
+//             'MINE',
+//             //silver
+//             'LOOM',
+//             'GLASSWORKS',
+//             'PRESS',
+//             // red first
+//             'STOCKADE',
+//             'BARRACKS',
+//             'GUARD_TOWER',
+//
+//             //red  second age
+//             'WALLS',
+//             'TRAINING_GROUND',
+//             'STABLES',
+//             'ARCHERY_RANGE',
+//             //red thierd age
+//
+//             'FORTIFICATIONS',
+//             'CIRCUS',
+//             'ARSENAL',
+//             'SIEGE_WORKSHOP',
+//             //first age gold
+//             'TAVERN',
+//             'EAST_TRADING_POST',
+//             'WEST_TRADING_POST',
+//             'MARKETPLACE',
+//             //second age gold
+//             'FORUM',
+//             'CARAVANSERY',
+//             'VINEYARD',
+//             'BAZAR',
+//             // thierd  age gold
+//             'HAVEN',
+//             'LIGHTHOUSE',
+//             'CHAMBER_OF_COMMERCE',
+//             'ARENA',
+//             // green irst age
+//             'WORKSHOP',
+//             'SCRIPTORIUM',
+//             'APOTHECARY',
+//             //green  second age
+//             'DISPENSARY',
+//             'LABORATORY',
+//             'LIBRARY',
+//             'SCHOOL',
+//             //green thierd age
+//             'LODGE',
+//             'OBSERVATORY',
+//             'UNIVERSITY',
+//             'ACADEMY',
+//             'STUDY',
+//
+//             // purple
+//             'WORKERS_GUILD',
+//             'CRAFTMENS_GUILD',
+//             'TRADERS_GUILD',
+//             'PHILOSOPHERS_GUILD',
+//             'SPY_GUILD',
+//             'STRATEGY_GUILD',
+//             'SHIPOWNERS_GUILD',
+//             'SCIENTISTS_GUILD',
+//             'MAGISTRATES_GUILD',
+//             'BUILDERS_GUILD');
+
+//    'THE_COLOSSUS_OF_RHODES',
+//            'THE_LIGHTHOUSE_OF_ALEXANDRIA',
+//            'THE_TEMPLE_OF_ARTEMIS_IN_EPHESUS',
+//            'THE_HANGING_GARDENS_OF_BABYLON',
+//            'THE_STATUE_OF_ZEUS_IN_OLYMPIA',
+//            'THE_MAUSOLEUM_OF_HALICARNASSUS',
+//            'THE_PYRAMIDS_OF_GIZA',
+//
+//            'LUMBER_YARD',
+//            'STONE_PIT',
+//            'CLAY_POOL',
+//            'ORE_VEIN',
+//
+//            'SAWMILL',
+//            'QUARRY',
+//            'BRICKYARD',
+//            'FOUNDRY',
+//
+//
+//            'TREE_FARM',
+//            'EXCAVATION',
+//            'CLAY_PIT',
+//            'TIMBER_YARD',
+//            'FOREST_CAVE',
+//            'MINE',
+//
+//            'LOOM',
+//            'GLASSWORKS',
+//            'PRESS',
+//
+//            'STOCKADE',
+//            'BARRACKS',
+//            'GUARD_TOWER',
+//
+//
+//            'WALLS',
+//            'TRAINING_GROUND',
+//            'STABLES',
+//            'ARCHERY_RANGE',
+//
+//            'FORTIFICATIONS',
+//            'CIRCUS',
+//            'ARSENAL',
+//            'SIEGE_WORKSHOP',
+//
+//            'TAVERN',
+//            'EAST_TRADING_POST',
+//            'WEST_TRADING_POST',
+//            'MARKETPLACE',
+//
+//            'FORUM',
+//            'CARAVANSERY',
+//            'VINEYARD',
+//            'BAZAR',
+//
+//            'HAVEN',
+//            'LIGHTHOUSE',
+//            'CHAMBER_OF_COMMERCE',
+//            'ARENA',
+//
+//            'WORKSHOP',
+//            'SCRIPTORIUM',
+//            'APOTHECARY',
+//
+//            'DISPENSARY',
+//            'LABORATORY',
+//            'LIBRARY',
+//            'SCHOOL',
+//
+//            'LODGE',
+//            'OBSERVATORY',
+//            'UNIVERSITY',
+//            'ACADEMY',
+//            'STUDY',
+//
+//
+//            'WORKERS_GUILD',
+//            'CRAFTMENS_GUILD',
+//            'TRADERS_GUILD',
+//            'PHILOSOPHERS_GUILD',
+//            'SPY_GUILD',
+//            'STRATEGY_GUILD',
+//            'SHIPOWNERS_GUILD',
+//            'SCIENTISTS_GUILD',
+//            'MAGISTRATES_GUILD',
+//            'BUILDERS_GUILD'
    }
