416728b424da5477a99939ba0c184c08770b7e3d
Pavlenko BOhdan <godexxxx@gmail.com>
2017-06-23 17:48:14 +0300

    add on build

---
+++ src/main/java/com/wonder/wonder/cards/event/on/build/ReceiveGoldOnBuildEventImpl.java
@@ -0,0 +1,28 @@
+package com.wonder.wonder.cards.event.on.build;
+
+import com.wonder.wonder.cards.ActionSide;
+import com.wonder.wonder.cards.GameCardColor;
+import lombok.Data;
+
+/**
+ * Created: godex
+ * DATE: 20.06.17.
+ */
+@Data
+public class ReceiveGoldOnBuildEventImpl implements OnBuildEvent {
+    private final ActionSide actionSide;
+    private final int pointForOneCard;
+    private final GameCardColor color;
+
+    public ReceiveGoldOnBuildEventImpl(ActionSide actionSide, int pointForOneCard, GameCardColor color) {
+        this.actionSide = actionSide;
+        this.pointForOneCard = pointForOneCard;
+        this.color = color;
+    }
+
+
+    @Override
+    public void doAction() {
+
+    }
+}
