d0f6665c0a82420854b6922bc15e312f094594d0
bm <b.missurenko@gmail.com>
2017-06-25 15:35:17 +0300

    add passiveAbility(dont rememder what Roma name say), add in events for diffrend gold card, refactor GameCard. in GameCard add field OnAbility for trade card.

--- src/main/java/com/wonder/wonder/cards/passiveAbility/OnBuildAbility.java
+++ src/main/java/com/wonder/wonder/cards/passiveAbility/OnBuildAbility.java
@@ -1,8 +1,23 @@
 package com.wonder.wonder.cards.passiveAbility;
 
+import com.wonder.wonder.cards.ActionSide;
+import com.wonder.wonder.cards.BaseResource;
+
+import java.util.List;
+
+
 /**
  * Created bm
  * Date 25.06.17.
  */
 public interface OnBuildAbility {
+    static OnBuildAbility none() {
+        return () -> {        };
+    }
+
+    static OnBuildAbility trade(ActionSide actionSide, List listBaseResource ) {
+        return new TradeOnBuildAbilityImpl(actionSide,listBaseResource);
+    }
+
+    void ability();
 }
