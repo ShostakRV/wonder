95fc06da7e0075cb9a58a2acf3a2952f26f248f0
B.missurenko <b.missurenko@gmail.com>
2017-07-16 15:55:32 +0300

    missurenko work plus Pavlenko work

--- src/test/java/com/wonder/wonder/WonderApplicationTests.java
+++ src/test/java/com/wonder/wonder/WonderApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class gitWonderApplicationTests {
+public class WonderApplicationTests {
 
 	@Test
 	public void contextLoads() {
