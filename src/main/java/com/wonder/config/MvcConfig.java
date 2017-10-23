5e0c8978e4d4f3a9f4a8372dcbb382dd7543a752
PavlenkoB <godexxxx@gmail.com>
2017-10-23 22:25:50 +0300

    disable web mvc default config

--- src/main/java/com/wonder/config/MvcConfig.java
+++ src/main/java/com/wonder/config/MvcConfig.java
@@ -10,7 +10,7 @@ import org.springframework.web.servlet.config.annotation.*;
  * Date: 03.06.17.
  */
 @Configuration
-@EnableWebMvc
+//@EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
@@ -33,7 +33,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
-        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
+//        registry.addResourceHandler("/css/**").addResourceLocations("/static/css/");
+//        registry.addResourceHandler("/js/**").addResourceLocations("/static/js/");
     }
 }
