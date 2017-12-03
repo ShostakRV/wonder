b624333730d257875388043c0ca334a45229e9b6
PavlenkoB <godexxxx@gmail.com>
2017-12-03 15:32:44 +0200

    save state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,38 @@ import React from 'react';
 
 import {connect} from 'react-redux';
 
-import {getTracks} from './actions/tracks';
-import Menu from './Menu';
-
 const App = () => {
     return (
         <div>
+            <div className="container-fluid">
+                <div className="col-xs-offset-2 col-xs-8">
+                    <div className="col-xs-12 bg-info">
+                        <div className="col-xs-2">ID</div>
+                        <div className="col-xs-8">players</div>
+                        <div className="col-xs-2">3 column</div>
+                    </div>
+                    <div className="col-xs-12 ">
+                        <div className="col-xs-2">1</div>
+                        <div className="col-xs-8">nik 1;nik 2;nik 3</div>
+                        <div className="col-xs-2">3 column</div>
+                    </div>
+                    <div className="col-xs-12 bg-info">
+                        <div className="col-xs-2">2</div>
+                        <div className="col-xs-8">nik 1;nik 2;nik 5</div>
+                        <div className="col-xs-2">3 column</div>
+                    </div>
+                    <div className="col-xs-12 ">
+                        <div className="col-xs-2">3</div>
+                        <div className="col-xs-8">nik 1;nik 2;nik 7</div>
+                        <div className="col-xs-2">3 column</div>
+                    </div>
+                    <div className="col-xs-12 bg-info">
+                        <div className="col-xs-2">4</div>
+                        <div className="col-xs-8">nik 1;nik 2;nik 9</div>
+                        <div className="col-xs-2">3 column</div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
